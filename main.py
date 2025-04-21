dc7c4c7b12df60dbb0032d1ae2671d9daa111fa0
Bach Nguyen <69bnguyen@gmail.com>
2025-04-21 20:56:52 +0700

    add tool to read file content

--- main.py
+++ main.py
@@ -36,30 +36,49 @@ def search_files(
     if len(file_paths) == 0:
         raise Exception("Empty list of files!")
 
-    file_names = []
+    out_file_paths = []
     for file_path in file_paths:
         file_name = Path(file_path).name
         if fuzz.ratio(search_str, file_name) >= match_threshold:
-            file_names.append(file_name)
+            out_file_paths.append(file_path)
 
     # print(f"search string: {search_str}\n\noutput: {file_names}")
 
-    return file_names
+    return file_paths
 
 
 @mcp.tool()
-def list_file_names(search_str: str) -> list[str] | str:
+def list_file_paths(search_str: str) -> list[str] | str:
     """
-    Fuzzy search through all markdown files in the Obsidian vault for the specified search string
+    Fuzzy search through all markdown files in the Obsidian vault for the specified search string.
     Args:
         search_str: the string to search for
 
     Returns:
-        list[str]: a list of file names that match the search string
+        list[str]: a list of file paths that match the search string
     """
     try:
         markdown_files = get_markdown_files(OBSIDIAN_PATH)
         return search_files(search_str, markdown_files)
+    except Exception as e:
+        return f"Error: {e}"
+
+
+@mcp.tool()
+def read_file(file_path: str) -> str:
+    """
+    Read a file and output its content.
+    Args:
+        file_path: the path of the file to read
+
+    Returns:
+        str: the content of the file
+    """
+    try:
+        with open(file_path, "r") as file:
+            content = file.read()
+
+        return content
     except Exception as e:
         return f"Error: {e}"
 
